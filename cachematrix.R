e66c335282cd742c8309e31863685c3bc4de9b5c
Pei Pang <pangpei11@gmail.com>
2017-10-21 17:48:36 -0400

    Update assignment 2
    Complete R code input

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,39 @@
 ## functions do
 
 ## Write a short comment describing this function
+## make a cacheable matrix
+## create an instance by calling makeCacheMatrix()
+## assign regular matrix to instance by calling $set stub <- matrix instance
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
+## solve or get cache of matrix inverse
+## pass makeCacheMatrix instance to the first argument
+## function returns solved inversion of matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
